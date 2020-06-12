ae85e87c4717273c95dfa110262d52224ddce84f
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-12 03:15:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const num1 = 31;
 const num2 = 2;
 const num3 = 29;
-let multiple = num1 * num2;
-let ran = Math.floor(Math.random() * 10) + 1;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
 let mod = 29%num3
 let max = Math.max(0,4,5,20)
